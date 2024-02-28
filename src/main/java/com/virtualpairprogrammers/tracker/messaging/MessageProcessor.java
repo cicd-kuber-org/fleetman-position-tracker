989e055aa57a32555601704d9ad94fbc044386ee
Loay Abdallati <Loai.Abdallatif@gmail.com>
2024-02-28 14:23:48 +0200

    newe speed value

--- src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
+++ src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
@@ -32,8 +32,9 @@ public class MessageProcessor {
 		VehiclePosition newReport = new VehicleBuilder()
 				                          .withName(incomingMessage.get("vehicle"))
 				                          .withLat(new BigDecimal(incomingMessage.get("lat")))
-				                          .withLng(new BigDecimal(incomingMessage.get("long")))
+				                          .withLng(new BigDecimal(incomingMessage.get("long")))                          
 				                          .withTimestamp(convertedDatestamp)
+							  .withSpeed(47.5)
 				                          .build();
 				                          
 		data.updatePosition(newReport);
